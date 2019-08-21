51a6194db733b9687202bde6affd29bc7a6d37b5
emaboy <virus-japones@hotmail.com>
2019-08-21 17:08:08 +0100

    Carregar todos os arquivos do projecto

---
+++ login.php
@@ -0,0 +1,16 @@
+<!-- Página: Login -->
+<!-- Autores: FEHF(Grupo nº: 0001) -->
+<!-- Descrição: Sistema de Informação de Saúde(SIS) -->
+<!-- Todos os direitos reservados a empresa FEHF 2019 -->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="_css/stylePrincipal.css"/>
+		<title>Sistema de Informação de Saúde(SIS)</title>
+	</head>
+	<body>
+		<h1>Sistema de Informação de Saúde(SIS)</h1>
+	</body>
+</html>
